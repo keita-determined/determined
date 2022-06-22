57a17b30e00c7f9350c89c51bd20121dd1bf4def
Maksim Kouznetsov <maksim.kouznetsov@hpe.com>
2022-06-22 10:25:17 -0700

    use non-ptl image in e2e tests

--- e2e_tests/tests/config.py
+++ e2e_tests/tests/config.py
@@ -15,11 +15,11 @@ MAX_TRIAL_BUILD_SECS = 90
 
 DEFAULT_TF1_CPU_IMAGE = "determinedai/environments:py-3.7-pytorch-1.7-tf-1.15-cpu-3e933ea"
 DEFAULT_TF2_CPU_IMAGE = (
-    "determinedai/environments:py-3.8-pytorch-1.10-lightning-1.5-tf-2.8-cpu-3e933ea"
+    "determinedai/environments-dev:py-3.8-pytorch-1.10-tf-2.8-cpu-251fac9"
 )
 DEFAULT_TF1_GPU_IMAGE = "determinedai/environments:cuda-10.2-pytorch-1.7-tf-1.15-gpu-3e933ea"
 DEFAULT_TF2_GPU_IMAGE = (
-    "determinedai/environments:cuda-11.3-pytorch-1.10-lightning-1.5-tf-2.8-gpu-3e933ea"
+    "determinedai/environments-dev:cuda-11.3-pytorch-1.10-tf-2.8-gpu-251fac9"
 )
 
 TF1_CPU_IMAGE = os.environ.get("TF1_CPU_IMAGE") or DEFAULT_TF1_CPU_IMAGE
